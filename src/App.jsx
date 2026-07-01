b03a7d58c424159ea25f3d55213836a5cabf75be
Anurag kumar <anuragkumar62070@gmail.com>
2026-07-01 22:56:26 +0530

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 import { Homepage } from './component/Homepage'
 import { Forecast } from './component/Forecast'
 import './App.css'
 
 function App() {
  return(
-  <BrowserRouter>
+  <HashRouter>
   <Routes>
     <Route path='/' element={<Homepage />} />
     <Route path='/forecast/:city' element={<Forecast />} />
   </Routes>
-  </BrowserRouter>
+  </HashRouter>
  )
 }
 
